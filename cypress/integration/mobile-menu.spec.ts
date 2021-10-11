d74241a456dc2827605b14400b8a80ea0202df26
Matias Meno <m@tias.me>
2021-10-11 21:52:56 +0200

    Add cypress

---
+++ cypress/integration/mobile-menu.spec.ts
@@ -0,0 +1,37 @@
+/// <reference types="cypress" />
+
+describe('mobile-menu', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000');
+    cy.viewport('iphone-6');
+    cy.window().then((win) => {
+      win.addEventListener('sveltekit:start', () => {
+        win.document.body.classList.add('hydrated');
+      });
+    });
+  });
+
+  it('does not contain sign in link', () => {
+    cy.get('.menu__actions a').contains('Sign in').should('not.be.visible');
+  });
+  it('should reveal sign in link when clicking hamburger button', () => {
+    cy.get('body.hydrated');
+    cy.get('header button.toggle').click();
+
+    cy.get('nav.mobile a')
+      .contains('Sign in')
+      .should('be.visible')
+      .should('have.prop', 'href')
+      .and('equal', 'https://plus.dropzone.dev/');
+  });
+  it('should hide sign in link when clicking X button', () => {
+    cy.get('body.hydrated');
+    cy.get('header button.toggle').click();
+
+    cy.get('nav.mobile a').contains('Sign in').should('be.visible');
+
+    cy.get('header nav button.close').click();
+
+    cy.get('nav.mobile a').contains('Sign in').should('not.be.visible');
+  });
+});
