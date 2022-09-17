33b69baa9aa532deb9a58c2152da9bfc1afb219b
Matias Meno <m@tias.me>
2022-09-17 16:24:58 +0200

    Add plus site

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -17,7 +17,7 @@
   description="Dropzone.js is an open source library that provides beautiful and easy to use drag'n'drop file uploads with image previews."
 />
 
-<Intro title="File uploads made easy" backgroundImage="js">
+<Intro title="File uploads made easy">
   <p>
     <strong>Dropzone.js</strong> is one of the most popular drag and drop
     JavaScript libraries. It is
--- src/routes/plus.svelte
+++ src/routes/plus.svelte
@@ -1,9 +1,11 @@
-<script context="module">
-  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-  export async function load() {
-    return {
-      status: 302,
-      redirect: '/',
-    }
-  }
+<script>
+  import Intro from '$lib/Intro.svelte'
 </script>
+
+<Intro title="Dropzone Plus has been discontinued">
+  <p>
+    We're sorry to inform you that we have made the hard decision to discontinue
+    Dropzone Plus.
+  </p>
+  <p>All active subscriptions have been refunded.</p>
+</Intro>
